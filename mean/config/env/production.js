b567026e4ad3ef4ff8070df8496ac6c31ae50cbb
Eric Marie <eric.marie18@gmail.com>
2015-05-13 10:58:45 +0200

    - Ajout des ensemble {id client / clé} de facebook, twitter et google dans la config de prod pour Heroku  - Mise à jour des vue configuration des comptes

--- mean/config/env/production.js
+++ mean/config/env/production.js
@@ -2,19 +2,19 @@
 
 module.exports = {
 	db: 'mongodb://mkg:my-kitchen-garden@ds061631.mongolab.com:61631/heroku_app36743859',
-	facebook: {
-		clientID: process.env.FACEBOOK_ID || 'APP_ID',
-		clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
-		callbackURL: '/auth/facebook/callback'
-	},
-	twitter: {
-		clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
-		clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
-		callbackURL: '/auth/twitter/callback'
-	},
-	google: {
-		clientID: process.env.GOOGLE_ID || 'APP_ID',
-		clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
-		callbackURL: '/auth/google/callback'
-	}
+    facebook: {
+        clientID: process.env.FACEBOOK_ID || '1659328634300477',
+        clientSecret: process.env.FACEBOOK_SECRET || 'a32d300f84b79b2c5ccd6befa21e644e',
+        callbackURL: '/auth/facebook/callback'
+    },
+    twitter: {
+        clientID: process.env.TWITTER_KEY || 'wawMFwa3WjYlwfj8HGf9FADeH',
+        clientSecret: process.env.TWITTER_SECRET || 'ImySvm0ko3wzU9tFyXBHzZ1BGmY57Vr73gDbJfs7ph3ct0OiPF',
+        callbackURL: '/auth/twitter/callback'
+    },
+    google: {
+        clientID: process.env.GOOGLE_ID || '292123444675-5fi8g4r3hkm33dsmk26818vivv8ciri8.apps.googleusercontent.com',
+        clientSecret: process.env.GOOGLE_SECRET || 'm-hlGDXv1lRf8tAEv7WQV3oR',
+        callbackURL: '/auth/google/callback'
+    }
 };
