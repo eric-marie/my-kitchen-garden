22d11f9ee44398839f5f5f5135295b7027052a7a
Eric Marie <eric.marie18@gmail.com>
2015-05-14 16:23:12 +0200

    - Réparation de uiToTop

--- assets/ReStart/js/ui-to-top.js
+++ assets/ReStart/js/ui-to-top.js
@@ -1,10 +1,15 @@
 (function($) {
     $(document).ready(function () {
-        $().UItoTop({easingType: 'easeOutQuart'});
+        window.ReStart = window.ReStart || {};
+        window.ReStart.uiToTop = function() {
+            $().UItoTop({easingType: 'easeOutQuart'});
 
-        if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
-            // Yep, it's Safari =)
-            $('.portfolio_item figcaption p').addClass('safari');
-        }
+            if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+                // Yep, it's Safari =)
+                $('.portfolio_item figcaption p').addClass('safari');
+            }
+        };
+
+        window.ReStart.uiToTop();
     });
 })(jQuery);
